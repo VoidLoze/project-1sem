865fd8184a59ae53f88750e78be90e6f63240a60
aleksandr <khomyakov.a.a.2004@gmail.com>
2023-06-05 18:11:29 +0300

    added model connection

---
+++ app/src/main/java/com/example/projet/avokado.java
@@ -0,0 +1,18 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class avokado extends AppCompatActivity {
+    private TextView avokado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avokado);
+
+    }
+}
